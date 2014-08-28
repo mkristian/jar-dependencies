4980cda8c5cd7ccad032e32472831b59f70b06c8
kares <self@kares.org>
2014-08-28 13:20:06 +0200

    make the private methods actually private - move around absolute and freeze_loading

--- lib/jar_dependencies.rb
+++ lib/jar_dependencies.rb
@@ -29,146 +29,152 @@ module Jars
   DEBUG = 'JARS_DEBUG'.freeze
   VENDOR = 'JARS_VENDOR'.freeze
 
-  if defined? JRUBY_VERSION
-    def self.to_prop( key )
-      java.lang.System.getProperty( key.downcase.gsub( /_/, '.' ) ) ||
+  class << self
+
+    if defined? JRUBY_VERSION
+      def to_prop( key )
+        java.lang.System.getProperty( key.downcase.gsub( /_/, '.' ) ) ||
+          ENV[ key.upcase.gsub( /[.]/, '_' ) ]
+      end
+    else
+      def to_prop( key )
         ENV[ key.upcase.gsub( /[.]/, '_' ) ]
-    end
-  else
-    def self.to_prop( key )
-      ENV[ key.upcase.gsub( /[.]/, '_' ) ]
-    end
-  end
-
-  def self.to_boolean( key )
-    prop = to_prop( key )
-    # prop == nil => false
-    # prop == 'false' => false
-    # anything else => true
-    prop == '' or prop == 'true'
-  end
-
-  def self.skip?
-    to_boolean( SKIP )
-  end
-
-  def self.no_require?
-    to_boolean( NO_REQUIRE )
-  end
-
-  def self.quiet?
-    to_boolean( QUIET )
-  end
-
-  def self.verbose?
-    to_boolean( VERBOSE )
-  end
-
-  def self.debug?
-    to_boolean( DEBUG )
-  end
-
-  def self.vendor?
-    to_boolean( VENDOR )
-  end
-
-  def self.absolute( file )
-    File.expand_path( file ) if file
-  end
-
-  def self.reset
-    @_jars_maven_settings_ = nil
-    @_jars_home_ = nil
-    @@jars ||= {}
-    @@jars.clear
-  end
-
-  def self.maven_settings
-    if @_jars_maven_settings_.nil?
-      unless @_jars_maven_settings_ = absolute( to_prop( MAVEN_SETTINGS ) )
-        # use maven default settings
-        @_jars_maven_settings_ = File.join( user_home, '.m2', 'settings.xml' )
       end
     end
-    @_jars_maven_settings_
-  end
 
-  def self.home
-    if @_jars_home_.nil?
-      unless @_jars_home_ = absolute( to_prop( HOME ) )
-        begin
-          require 'rexml/document'
-          doc = REXML::Document.new( File.read( maven_settings ) )
-          REXML::XPath.first( doc, "//settings/localRepository").tap do |e|
-            @_jars_home_ = e.text.sub( /\\/, '/') if e
-          end
-        rescue
-          # ignore
+    def to_boolean( key )
+      prop = to_prop( key )
+      # prop == nil => false
+      # prop == 'false' => false
+      # anything else => true
+      prop == '' or prop == 'true'
+    end
+
+    def skip?
+      to_boolean( SKIP )
+    end
+
+    def no_require?
+      to_boolean( NO_REQUIRE )
+    end
+
+    def quiet?
+      to_boolean( QUIET )
+    end
+
+    def verbose?
+      to_boolean( VERBOSE )
+    end
+
+    def debug?
+      to_boolean( DEBUG )
+    end
+
+    def vendor?
+      to_boolean( VENDOR )
+    end
+
+    def freeze_loading
+      ENV[ NO_REQUIRE ] = 'true'
+    end
+
+    def reset
+      @_jars_maven_settings_ = nil
+      @_jars_home_ = nil
+      @@jars ||= {}
+      @@jars.clear
+    end
+
+    def maven_settings
+      if @_jars_maven_settings_.nil?
+        unless @_jars_maven_settings_ = absolute( to_prop( MAVEN_SETTINGS ) )
+          # use maven default settings
+          @_jars_maven_settings_ = File.join( user_home, '.m2', 'settings.xml' )
         end
       end
-      # use maven default repository
-      @_jars_home_ ||= File.join( user_home, '.m2', 'repository' )
+      @_jars_maven_settings_
     end
-    @_jars_home_
-  end
 
-  def self.require_jar( group_id, artifact_id, *classifier_version )
-    version = classifier_version[ -1 ]
-    classifier = classifier_version[ -2 ]
+    def home
+      if @_jars_home_.nil?
+        unless @_jars_home_ = absolute( to_prop( HOME ) )
+          begin
+            require 'rexml/document'
+            doc = REXML::Document.new( File.read( maven_settings ) )
+            REXML::XPath.first( doc, "//settings/localRepository").tap do |e|
+              @_jars_home_ = e.text.sub( /\\/, '/') if e
+            end
+          rescue
+            # ignore
+          end
+        end
+        # use maven default repository
+        @_jars_home_ ||= File.join( user_home, '.m2', 'repository' )
+      end
+      @_jars_home_
+    end
 
-    @@jars ||= {}
-    coordinate = "#{group_id}:#{artifact_id}"
-    coordinate += ":#{classifier}" if classifier
-    if @@jars.key? coordinate
-      if @@jars[ coordinate ] == version
-        false
+    def require_jar( group_id, artifact_id, *classifier_version )
+      version = classifier_version[ -1 ]
+      classifier = classifier_version[ -2 ]
+
+      @@jars ||= {}
+      coordinate = "#{group_id}:#{artifact_id}"
+      coordinate += ":#{classifier}" if classifier
+      if @@jars.key? coordinate
+        if @@jars[ coordinate ] == version
+          false
+        else
+          # version of already registered jar
+          @@jars[ coordinate ]
+        end
       else
-        # version of already registered jar
-        @@jars[ coordinate ]
+        do_require( group_id, artifact_id, version, classifier )
+        @@jars[ coordinate ] = version
+        return true
       end
-    else
-      do_require( group_id, artifact_id, version, classifier )
-      @@jars[ coordinate ] = version
-      return true
     end
-  end
 
-  private
+    private
 
-  def self.user_home
-    ENV[ 'HOME' ] || begin
-      user_home = Dir.home if Dir.respond_to?(:home)
-      unless user_home
-        user_home = ENV_JAVA[ 'user.home' ] if Object.const_defined?(:ENV_JAVA)
+
+    def absolute( file )
+      File.expand_path( file ) if file
+    end
+
+    def user_home
+      ENV[ 'HOME' ] || begin
+        user_home = Dir.home if Dir.respond_to?(:home)
+        unless user_home
+          user_home = ENV_JAVA[ 'user.home' ] if Object.const_defined?(:ENV_JAVA)
+        end
+        user_home
       end
-      user_home
     end
-  end
 
-  def self.to_jar( group_id, artifact_id, version, classifier )
-    file = "#{group_id.gsub( /\./, '/' )}/#{artifact_id}/#{version}/#{artifact_id}-#{version}"
-    file << "-#{classifier}" if classifier
-    file << '.jar'
-    file
-  end
-
-  def self.do_require( *args )
-    jar = to_jar( *args )
-    file = File.join( home, jar )
-    # use jar from local repository if exists
-    if File.exists?( file )
-      require file
-    else
-      # otherwise try to find it on the load path
-      require jar
+    def to_jar( group_id, artifact_id, version, classifier )
+      file = "#{group_id.gsub( /\./, '/' )}/#{artifact_id}/#{version}/#{artifact_id}-#{version}"
+      file << "-#{classifier}" if classifier
+      file << '.jar'
+      file
     end
-  rescue LoadError => e
-    raise "\n\n\tyou might need to reinstall the gem which depends on the missing jar\n\n" + e.message + " (LoadError)"
-  end
 
-  def self.freeze_loading
-    ENV[ NO_REQUIRE ] = 'true'
-  end
+    def do_require( *args )
+      jar = to_jar( *args )
+      file = File.join( home, jar )
+      # use jar from local repository if exists
+      if File.exists?( file )
+        require file
+      else
+        # otherwise try to find it on the load path
+        require jar
+      end
+    rescue LoadError => e
+      raise "\n\n\tyou might need to reinstall the gem which depends on the missing jar\n\n" + e.message + " (LoadError)"
+    end
+
+  end # class << self
+
 end
 
 def require_jar( *args )
