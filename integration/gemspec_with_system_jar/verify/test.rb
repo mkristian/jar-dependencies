fa7ec134680cbb98a6c406dcba13659c0c875acd
Christian Meier <m.kristian@web.de>
2015-02-05 12:06:21 +0100

    try to gemspec_with_system_jar test more robust

--- integration/gemspec_with_system_jar/verify/test.rb
+++ integration/gemspec_with_system_jar/verify/test.rb
@@ -1,5 +1,11 @@
 #-*- mode: ruby -*-
 
+spec = eval( File.read( File.expand_path('../../../../jar-dependencies.gemspec', __FILE__ ) ) )
+
+# force to use prereleased gem
+gem 'jar-dependencies', spec.version
+require 'jar-dependencies'
+
 Gem.install( File.expand_path( "../../gem/pkg/first-1.1.1.gem", __FILE__ ) )
 
 require "first"
