2053b0046a010d5e25ab0dccb8a1c74c01bc612d
Christian Meier <m.kristian@web.de>
2015-07-12 18:44:02 +0200

    switch to jruby-1.7.21 and improve ITs

---
+++ integration/install_ruby_maven_when_needed/verify/test_bundler.rb
@@ -0,0 +1,17 @@
+gemspecs = File.join( Gem.dir, 'specifications', 'ruby-maven-*gemspec' )
+Dir[ gemspecs ].each do |f|
+  File.delete( f ) rescue nil
+end
+
+version = ARGV[0]
+gem "jar-dependencies", version.sub(/-SNAPSHOT/,"")
+
+Kernel.at_exit do
+  if Dir[ gemspecs ].size != 2
+    raise "did not find two ruby-maven gems installed #{Dir[ gemspecs ]}"
+  end
+end
+
+# this is like: gem install --ignore-dependencies, ../gem/pkg/my-1.1.1.gem
+ARGV.replace( ['install'] )
+load File.join(Gem.bindir, "bundle")
