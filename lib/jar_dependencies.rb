cf90777ea6546cb818948e29badea5c173ed8af7
kares <self@kares.org>
2017-01-13 09:52:44 +0100

    delay loading of MavenSettings until used

--- lib/jar_dependencies.rb
+++ lib/jar_dependencies.rb
@@ -18,7 +18,7 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
-require 'jars/maven_settings'
+
 module Jars
   unless defined? Jars::SKIP_LOCK
     MAVEN_SETTINGS = 'JARS_MAVEN_SETTINGS'.freeze
@@ -46,6 +46,8 @@ module Jars
     # vendor jars inside gem when installing gem
     VENDOR = 'JARS_VENDOR'.freeze
   end
+
+  autoload :MavenSettings, 'jars/maven_settings'
 
   class << self
 
